baa7bfbf5d39ea110e9d40a517d35894f055448c
lavanya-62 <lavanyas2266@gmail.com>
2026-09-05 16:13:46 +0530

    Fix Aiven MySQL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,10 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
